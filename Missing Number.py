b61d5c8cceb62a4cc110221bb86206fc61a00df6
Harithas <harithas201@gmail.com>
2020-05-20 14:20:04 +0530

    practise files

---
+++ Missing Number.py
@@ -0,0 +1,19 @@
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+# Example 1:
+
+# Input: [3,0,1]
+# Output: 2
+# Example 2:
+
+# Input: [9,6,4,2,3,5,7,0,1]
+# Output: 8
+
+class Solution:
+    def missingNumber(self, nums):
+        num_set = set(nums)
+        print(num_set)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in num_set:
+                return number
